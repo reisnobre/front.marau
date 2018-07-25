7ca1c0cb482134ef946e52430ed55ecd14604283
Eduardo Reis Nobre <eduardo.reisnobre@gmail.com>
2018-07-25 11:20:53 -0300

    init

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="app">
+    <transition name="fade" mode="out-in">
+      <router-view :scroll="scroll"></router-view>
+    </transition>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data () {
+    return {
+      scroll: 0
+    }
+  },
+  mounted () {
+  },
+  components: {
+  }
+}
+</script>
+
+<style lang="scss">
+  @import "assets/scss/main.scss";
+</style>
